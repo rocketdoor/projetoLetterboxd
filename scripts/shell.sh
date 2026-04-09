be8b091de007823261d8df28a6de31e2e9719c6c
Rômulo Cabral de Magalhães <160641833+rocketdoor@users.noreply.github.com>
2026-04-09 19:56:51 -0300

    configurando email

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,4 +1,4 @@
 pwd
 false
 sudo apt-get install -y mailutils
-echo "Mandando email com mailutils do linux" | mail -s "Teste de email" "$RECIPIENT_EMAIL"
+echo "Mandando email com mailutils do linux, seu codigo deu certo!" | mail -s "Teste de email" "$RECIPIENT_EMAIL"
